174e2a9c9b8ddd233f02df7a1e53424c6f20c474
Nicholas Lim <nicknitewolf@lineageos.org>
2018-07-22 20:16:41 +0800

    tissot: Fix up A/B support
     * bootctrl from https://github.com/LineageOS/android_hardware_qcom_bootctrl
     * gpt-utils from https://github.com/LineageOS/android_device_xiaomi_tissot
    
    Change-Id: Id28522337507ddda3fd5ab539130b33e9fc70b29

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,6 +40,9 @@ TARGET_NO_BOOTLOADER := true
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
 
+# GPT Utils
+BOARD_PROVIDES_GPTUTILS := true
+
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 androidboot.selinux=permissive
--- omni_tissot.mk
+++ omni_tissot.mk
@@ -19,6 +19,45 @@ $(call inherit-product, vendor/omni/config/common.mk)
 
 $(call inherit-product, build/target/product/embedded.mk)
 
+# A/B
+AB_OTA_UPDATER := true
+
+AB_OTA_PARTITIONS += \
+    boot \
+    system
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script
+
+# Boot control
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service \
+    bootctrl.msm8953 \
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
+
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    bootctrl.msm8953 \
+    libgptutils \
+    libz
+
+# Update engine
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
 # Time Zone data for recovery
 PRODUCT_COPY_FILES += \
     system/timezone/output_data/iana/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
