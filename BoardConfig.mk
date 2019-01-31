e9ef798c90a2b23c9f573109955d26b61c89e091
dianlujitao <dianlujitao@lineageos.org>
2019-01-31 22:26:17 +0800

    tissot: Use libcryptfs_hw from commonsys repo
    Change-Id: I342f57e9e66cd4f6674d7ce318a2a8dabebef8c2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,6 +38,7 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8953
 TARGET_NO_BOOTLOADER := true
 
 # Crypto
+TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
 TARGET_HW_DISK_ENCRYPTION := true
 
 # Encryption
