05e2b9c9a4f94e95635e3e048bd21d5a31c643fd
LinkBoi <50962670+LinkBoi00@users.noreply.github.com>
2020-12-06 14:40:59 +0200

    daisy: Rework prepdecrypt script
    * Use magiskboot to get the os version and security patch values from boot, instead of reading them from /system/build.prop
    * Fixes decryption in GSIs

--- recovery/root/sbin/prepdecrypt.sh
+++ recovery/root/sbin/prepdecrypt.sh
@@ -1,33 +1,23 @@
 #!/sbin/sh
 
+tmp="/dev/tmp"
+tool="magiskboot"
 suffix=$(getprop ro.boot.slot_suffix)
-if [ -z "$suffix" ]; then
-    suf=$(getprop ro.boot.slot)
-    suffix="_$suf"
-fi
-
-venpath="/dev/block/bootdevice/by-name/vendor$suffix"
-mkdir /v
-mount -t ext4 -o ro "$venpath" /v
-
-syspath="/dev/block/bootdevice/by-name/system$suffix"
-mkdir /s
-mount -t ext4 -o ro "$syspath" /s
-
+bootpath="/dev/block/bootdevice/by-name/boot$suffix"
 is_fastboot_twrp=$(getprop ro.boot.fastboot)
-venpatchlevel=$(grep -i 'ro.vendor.build.security_patch=' "/v/build.prop"  | cut -f2 -d'=' -s)
-osver=$(grep -i 'ro.build.version.release=' "/s/system/build.prop"  | cut -f2 -d'=' -s)
-patchlevel=$(grep -i 'ro.build.version.security_patch=' "/s/system/build.prop"  | cut -f2 -d'=' -s)
 
-if [ "$is_fastboot_twrp" != "1" ]; then   
-    resetprop "ro.vendor.build.security_patch" "$venpatchlevel"
-    resetprop "ro.build.version.release" "$osver"
-    resetprop "ro.build.version.security_patch" "$patchlevel"
+if [ "$is_fastboot_twrp" != "1" ]; then 
+	mkdir -p "$tmp" && cd "$tmp" && cp -f "/sbin/$tool" "$tmp" && chmod 755 "$tool"
+	$tool unpack -h "$bootpath" # Unpack the boot image to the working directory
+	# Read the needed values from the boot header
+	osver=$(grep -i 'os_version=' "$tmp/header"  | cut -f2 -d'=' -s)
+	patchlevel=$(grep -i 'os_patch_level=' "$tmp/header"  | cut -f2 -d'=' -s)
+	patchlevel="$patchlevel-01" # Add "-01" at the end of $patchlevel because it's needed
+	# Set the OS version and security patch level props to the values taken from the boot header to make decryption (in installed TWRP) work
+	resetprop "ro.build.version.release" "$osver"
+	resetprop "ro.build.version.security_patch" "$patchlevel"
+	$tool cleanup && cd "/" && rm -rf "$tmp" # Delete the working directory
 fi
 
-umount /v
-umount /s
-rmdir /v
-rmdir /s
-setprop crypto.ready 1
+setprop crypto.ready 1 # Set crypto.ready to 1 so that the decryption services can start
 exit 0
