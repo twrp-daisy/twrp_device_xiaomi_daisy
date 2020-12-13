0fdbb036f6415db23e76599389eed45a4daf2041
LinkBoi <50962670+LinkBoi00@users.noreply.github.com>
2020-12-13 12:43:42 +0200

    daisy: Build more files from source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,6 +95,18 @@ TW_USE_TOOLBOX := true
 TW_INCLUDE_REPACKTOOLS := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libicuuc \
+    libxml2 \
+    libion \
+    android.hidl.base@1.0
+
+TW_RECOVERY_ADDITIONAL_RELINK_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libicuuc.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT)/lib64/android.hidl.base@1.0.so
+
 # Recovery Installer
 USE_RECOVERY_INSTALLER := true
 RECOVERY_INSTALLER_PATH := $(LOCAL_PATH)/installer
