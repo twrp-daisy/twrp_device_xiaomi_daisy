92eee69d4bf83459adc6ffa4950e5d329f0c6e38
ItsVixano <vixanonew@gmail.com>
2021-02-01 10:51:00 +0000

    daisy: Set ro.treble.enabled on true
    * Treble is supported by our device
    
    Signed-off-by: LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>

--- omni_daisy.mk
+++ omni_daisy.mk
@@ -61,7 +61,8 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.keystore=msm8953 \
     ro.hardware.gatekeeper=msm8953 \
-    ro.hardware.bootctrl=msm8953
+    ro.hardware.bootctrl=msm8953 \
+	ro.treble.enabled=true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := daisy
